e652b1e90620c6e6f7326ec8d53da71c75a818e1
roryok <roryok@gmail.com>
2017-09-09 03:48:09 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -10,7 +10,7 @@ module.exports = {
     let ip = request.connection.remoteAddress;
     ip = (ip == '127.0.0.1' ? '77.95.165.9' : ip)
     let url = `https://freegeoip.net/json/${ip}`
-    console.log(url, request.connection);
+    return reply(JSON.stringify( { url, request.connection } ));
     Req(url, (err, response, json) => {
       if (err) throw err;
       return reply(json.replace('Connaught','Connacht'));             
